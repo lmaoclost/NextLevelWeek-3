8c4966c02d4cec2c6e519b634bb704e7d8673b6e
lmaoclost <renansmoliveira@gmail.com>
2020-10-12 15:24:08 -0300

    Initial React APP

---
+++ web/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg';
+
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Campos dos Goytacazes</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <a href="" className="enter-app">></a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
